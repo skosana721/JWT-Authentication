e04d8f3e536b25017ac7bcc1f6ca4cd0ed94cd3a
skosana721 <wonganiskosana@gmail.com>
2021-08-12 12:03:18 +0200

    aded get and a post request for with a path "/register"

--- auth/server/routes/api/user.js
+++ auth/server/routes/api/user.js
@@ -1,9 +1,26 @@
+const User = require("../../model/user");
+
 const userRoutes = (app) => {
-  app.get("/user", (req, res) => {
-    res.send("Users");
+  app.get("/user", async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).send(users);
+    } catch (error) {
+      res.status(404).send(error);
+    }
   });
-  app.post("/user", (req, res) => {
-    res.send("post");
+  app.post("/register", async (req, res) => {
+    try {
+      const newUser = await User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      }).save();
+
+      res.status(201).send(newUser);
+    } catch (error) {
+      res.status(400).send(`${error.keyValue.email} already exists`);
+    }
   });
 };
 
