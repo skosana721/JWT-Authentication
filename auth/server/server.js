4763aa820a80ae7eaaf04a82d98a275c2e4a8670
skosana721 <wonganiskosana@gmail.com>
2021-08-12 11:31:44 +0200

    required mongoose and connect to mongodb database and used expess bodyparser

--- auth/server/server.js
+++ auth/server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const userRoutes = require("./routes/api/user");
+require("dotenv/config");
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 userRoutes(app);
 
 const port = 5000;
