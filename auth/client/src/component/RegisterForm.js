841efd3f0aa7ba6c44fb9e5c6992d4f6dadae63f
skosana721 <wonganiskosana@gmail.com>
2021-08-13 11:38:14 +0200

    created a register form which has inputs for username email, email, password, confirm password and sign in button

--- auth/client/src/component/RegisterForm.js
+++ auth/client/src/component/RegisterForm.js
@@ -1,7 +1,77 @@
 import React from "react";
-
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+import useForm from "./useForm";
 const RegisterForm = () => {
-  return <div></div>;
+  const { handleChange, handleSubmit, formInfo } = useForm();
+
+  return (
+    <Container>
+      <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <div>
+            <h2>Welcome</h2>
+            <span>Let's create your account!</span>
+            <Form onSubmit={handleSubmit}>
+              <FormGroup>
+                <Label for="username">Username</Label>
+                <Input
+                  type="text"
+                  name="username"
+                  onChange={handleChange}
+                  id="username"
+                  value={formInfo.username}
+                  placeholder="Enter username"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  id="email"
+                  value={formInfo.email}
+                  placeholder="Enter email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  id="password"
+                  value={formInfo.password}
+                  placeholder="Enter password"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="confirmPassword">Confirm Password</Label>
+                <Input
+                  type="password"
+                  name="confirmPassword"
+                  onChange={handleChange}
+                  id="confirmPassword"
+                  value={formInfo.confirmPassword}
+                  placeholder="Confirm password"
+                />
+              </FormGroup>
+              <Button color="primary">Sign up</Button>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default RegisterForm;
