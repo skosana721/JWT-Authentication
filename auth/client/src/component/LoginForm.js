bcf47567aad4f189dcbcee4e697a3323684cb29b
skosana721 <wonganiskosana@gmail.com>
2021-08-13 11:39:00 +0200

    created a login form which has inputs for email, password and login button

--- auth/client/src/component/LoginForm.js
+++ auth/client/src/component/LoginForm.js
@@ -1,7 +1,55 @@
 import React from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+import useForm from "./useForm";
 
 const LoginForm = () => {
-  return <div></div>;
+  const { handleInputs, handleLogIn, loginForm } = useForm();
+
+  return (
+    <Container>
+      <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <div>
+            <span>Sign in to your account</span>
+            <Form onSubmit={handleLogIn}>
+              <FormGroup>
+                <Label for="LogInEmail">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  onChange={handleInputs}
+                  id="LogInEmail"
+                  value={loginForm.email}
+                  placeholder="Enter email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="logInPassword">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  onChange={handleInputs}
+                  id="logInPassword"
+                  value={loginForm.password}
+                  placeholder="Enter password"
+                />
+              </FormGroup>
+              <Button>Sign in</Button>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default LoginForm;
