10bcecf4476ef3a38ac20b8e401bdfc5e3973ec7
skosana721 <wonganiskosana@gmail.com>
2021-08-17 14:23:13 +0200

    required loginValidation and registerationValidation

--- auth/server/routes/api/user.js
+++ auth/server/routes/api/user.js
@@ -1,4 +1,6 @@
 const User = require("../../model/user");
+const { registerValidation, loginValidation } = require("../../validation");
+const bcrypt = require("bcrypt");
 
 const userRoutes = (app) => {
   app.get("/user", async (req, res) => {
@@ -10,17 +12,46 @@ const userRoutes = (app) => {
     }
   });
   app.post("/register", async (req, res) => {
+    // valid before make a new user
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //check if a user exists
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send(`Email already exists`);
+
+    //password hashing
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     try {
       const newUser = await User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: hashPassword,
       }).save();
 
       res.status(201).send(newUser);
     } catch (error) {
-      res.status(400).send(`${error.keyValue.email} already exists`);
+      res.status(400).send(error);
     }
+  });
+  app.post("/login", async (req, res) => {
+    //validating user
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // check if user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send(`Email or password is wrong`);
+
+    // check if the password is correct
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) return res.status(400).send("Invalid password");
+    res.send("Logged in!");
   });
 };
 
