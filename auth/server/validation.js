cb749319624ff216bd916aa4fad72dcdeabe4775
skosana721 <wonganiskosana@gmail.com>
2021-08-17 14:19:51 +0200

    removed the validation for username in loginValidation

--- auth/server/validation.js
+++ auth/server/validation.js
@@ -12,7 +12,6 @@ const registerValidation = (data) => {
 
 const loginValidation = (data) => {
   const schema = Joi.object({
-    username: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
   });
