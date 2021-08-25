a4ab5d0bf96df7910df29dc5f93b5f1eeab0cff1
skosana721 <wonganiskosana@gmail.com>
2021-08-25 15:54:45 +0200

    added a condition before submitting

--- auth/client/src/component/useForm.js
+++ auth/client/src/component/useForm.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser, login } from "../redux/actions/auth";
+import { CLEAR_ERRORS } from "../redux/actionTypes/user";
 
 const useForm = () => {
+  const dispatch = useDispatch();
   const [formInfo, setFormInfo] = useState({
     username: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
   });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormInfo({ ...formInfo, [name]: value });
@@ -21,9 +25,18 @@ const useForm = () => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (formInfo.password) {
+      dispatch(registerUser(formInfo));
+      dispatch({ type: CLEAR_ERRORS });
+    }
   };
   const handleLogIn = (e) => {
     e.preventDefault();
+
+    if (loginForm.email) {
+      dispatch(login(loginForm));
+      dispatch({ type: CLEAR_ERRORS });
+    }
   };
   return {
     handleChange,
