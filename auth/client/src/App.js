8021edb285784bc3b11f183fe8b95ea197df4c32
skosana721 <wonganiskosana@gmail.com>
2021-08-26 08:03:49 +0200

    imported the loadUser action and dispatched the action when the app render

--- auth/client/src/App.js
+++ auth/client/src/App.js
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import DashBoard from "./component/DashBoard";
 import LoginForm from "./component/LoginForm";
+
 import RegisterForm from "./component/RegisterForm";
+import { loadUser } from "./redux/actions/auth";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUser());
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <h2>JWT Authentication</h2>
       </header>
+
       <DashBoard />
       <RegisterForm />
       <LoginForm />
