abdaf229d6c3f4ece32baa142502b9a3afaa9db0
skosana721 <wonganiskosana@gmail.com>
2021-08-26 11:57:26 +0200

    imported the Router, Route, Switch and privateRoute component and wrapped the components with Router component

--- auth/client/src/App.js
+++ auth/client/src/App.js
@@ -3,9 +3,11 @@ import { useDispatch } from "react-redux";
 import "./App.css";
 import DashBoard from "./component/DashBoard";
 import LoginForm from "./component/LoginForm";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RegisterForm from "./component/RegisterForm";
 import { loadUser } from "./redux/actions/auth";
+import PrivateRoute from "./routes/PrivateRoute";
+import Home from "./component/Home";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,14 +15,15 @@ function App() {
     dispatch(loadUser());
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>JWT Authentication</h2>
-      </header>
-
-      <DashBoard />
-      <RegisterForm />
-      <LoginForm />
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <PrivateRoute path="/Dashboard" component={DashBoard} />
+          <Route path="/user/register" component={RegisterForm} />
+          <Route path="/user/login" component={LoginForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
