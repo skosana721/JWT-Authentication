b0f9ae035716cd0094f08fdb6a714a03116e5cf9
skosana721 <wonganiskosana@gmail.com>
2021-08-26 07:39:44 +0200

    created a private route component

---
+++ auth/client/src/routes/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/register" />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
