5b6b48b60e13afe596cc2d3ae0153312f6a4a554
skosana721 <wonganiskosana@gmail.com>
2021-08-26 14:27:55 +0200

    created function that dispatch the logout action and also returns the user to the home page

--- auth/client/src/component/Logout.js
+++ auth/client/src/component/Logout.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Button } from "reactstrap";
 import { logout } from "../redux/actions/auth";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
 
 const Logout = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
+  const loggingOutUser = () => {
+    dispatch(logout());
+    history.push("/");
+  };
+
   return (
     <div>
-      <Button onClick={() => dispatch(logout())}>Logout</Button>
+      <Button onClick={() => loggingOutUser()}>Logout</Button>
     </div>
   );
 };
